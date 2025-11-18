a22f73d5e9ce66a832337ff6f7ed4412b37c046b
c3ak <mccational@gmail.com>
2025-11-18 16:06:04 +0900

    Update VN_Engine_M_test.user.js

--- VN_Engine_M_test.user.js
+++ VN_Engine_M_test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Visual Novel Engine V1.5_mobile_TEST (Landscape)
 // @namespace    http://tampermonkey.net/
-// @version      1.5-mobile
+// @version      1.5.2-mobile
 // @description  모바일 가로 모드에 최적화된 비주얼 노벨 엔진입니다.
 // @author       You & AI Assistant
 // @match        *://crack.wrtn.ai/*
